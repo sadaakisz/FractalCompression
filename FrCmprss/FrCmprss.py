f442ac4f24e19e04d68cd22b0459d5e5e04282f7
sadaakisz <hssm2000.sadaaki@gmail.com>
2019-09-27 10:17:16 -0500

    Combinacion

--- FrCmprss/FrCmprss.py
+++ FrCmprss/FrCmprss.py
@@ -10,49 +10,44 @@ import math
 # s es el contraste y b el brillo
 # flip y rotate son transformaciones junto con reduce
 
-# Manipulate channels
+# Manipulación de canales de color
 
-def get_greyscale_image(img): # tonos de grises (entre blanco y negro 256) del RGB
+def escala_grises(img): # tonos de grises (entre blanco y negro 256) del RGB
     return np.mean(img[:,:,:2], 2) # .mean devuelve el promedio de un array
 
-def extract_rgb(img): # extraemos los colores RGB verde, rojo y azul
+def extraer_rgb(img): # extraemos los colores RGB verde, rojo y azul
     return img[:,:,0], img[:,:,1], img[:,:,2] #[:, :, 0] = Verde [:, :, 1] = Azul [:, :, 2] = Rojo
 
-def assemble_rbg(img_r, img_g, img_b):
-    shape = (img_r.shape[0], img_r.shape[1], 1)
-    return np.concatenate((np.reshape(img_r, shape), np.reshape(img_g, shape), #une una secuencia de arrays con un axis existente
-        np.reshape(img_b, shape)), axis=2) # .reshape cambia la forma del array
+def construir_rgb(img_r, img_g, img_b):
+    forma = (img_r.shape[0], img_r.shape[1], 1)
+    return np.concatenate((np.reshape(img_r, forma), np.reshape(img_g, forma), #une una secuencia de arrays con un axis existente
+        np.reshape(img_b, forma)), axis=2) # .reshape cambia la forma del array
 
-# Transformations
-def reduce(img, factor): # reduce la imagen por el promedio de cada elemento
-    result = np.zeros((img.shape[0] // factor, img.shape[1] // factor)) #retorna nuevo array del tamaño y tipo indicado, llenado de 0s
-    for i in range(result.shape[0]):
-        for j in range(result.shape[1]):
-            result[i,j] = np.mean(img[i*factor:(i+1)*factor,j*factor:(j+1)*factor]) #promedio del array (reduce el tamaño del la imagen)
-    return result
+# Transformaciones
 
-def rotate(img, angle): # rotas la imagen
-    return ndimage.rotate(img, angle, reshape=False) # se rota por el angulo dado como parametro (para preservar la forma de la imagen el angulo tiene que estar entre {0, 90, 180, 270})
+def reducir(img, factor): # reduce la imagen por el promedio de cada elemento
+    resultado = np.zeros((img.shape[0] // factor, img.shape[1] // factor)) #retorna nuevo array del tamaño y tipo indicado, llenado de 0s
+    for i in range(resultado.shape[0]):
+        for j in range(resultado.shape[1]):
+            resultado[i,j] = np.mean(img[i*factor:(i+1)*factor,j*factor:(j+1)*factor]) #promedio del array (reduce el tamaño del la imagen)
+    return resultado
 
-def flip(img, direction): # voltea imagen
-    return img[::direction,:] # rota si es -1 y no hace nada si es 1
+def rotar(img, angulo): # rotas la imagen
+    return ndimage.rotate(img, angulo, reshape=False) # se rota por el angulo dado como parametro (para preservar la forma de la imagen el angulo tiene que estar entre {0, 90, 180, 270})
 
-def apply_transformation(img, direction, angle, contrast=1.0, brightness=0.0): # aplica la transformacion
-    return contrast*rotate(flip(img, direction), angle) + brightness #1 bit del flip, 2 bits del angulo, 8 para el contraste y la brillo
+def invertir(img, direccion): # voltea imagen
+    return img[::direccion,:] # rota si es -1 y no hace nada si es 1
 
-# Contrast and brightness
-def find_contrast_and_brightness1(D, S):
-    # Fix the contrast and only fit the brightness
-    contrast = 0.75
-    brightness = (np.sum(D - contrast*S)) / D.size
-    return contrast, brightness
+def aplicar_transformacion(img, direccion, angulo, contraste=1.0, brillo=0.0): # aplica la transformacion
+    return contraste*rotar(invertir(img, direccion), angulo) + brillo #1 bit del flip, 2 bits del angulo, 8 para el contraste y la brillo
 
-def find_contrast_and_brightness2(D, S):
-    # Fit the contrast and the brightness
+# Contraste y brillo
+
+def encontrar_brillo_contraste(D, S):
+    # Arreglar el contraste y brillo
     A = np.concatenate((np.ones((S.size, 1)), np.reshape(S, (S.size, 1))), axis=1)
     b = np.reshape(D, (D.size,))
     x, _, _, _ = np.linalg.lstsq(A, b)
-    #x = optimize.lsq_linear(A, b, [(-np.inf, -2.0), (np.inf, 2.0)]).x
     return x[1], x[0]
 
 # Compression for greyscale images
@@ -63,10 +58,10 @@ def generate_all_transformed_blocks(img, source_size, destination_size, step):
     for k in range((img.shape[0] - source_size) // step + 1):
         for l in range((img.shape[1] - source_size) // step + 1):
             # Extract the source block and reduce it to the shape of a destination block
-            S = reduce(img[k*step:k*step+source_size,l*step:l*step+source_size], factor)
+            S = reducir(img[k*step:k*step+source_size,l*step:l*step+source_size], factor)
             # Generate all possible transformed blocks
             for direction, angle in candidates:
-                transformed_blocks.append((k, l, direction, angle, apply_transformation(S, direction, angle)))
+                transformed_blocks.append((k, l, direction, angle, aplicar_transformacion(S, direction, angle)))
     return transformed_blocks
 
 def compress(img, source_size, destination_size, step):
@@ -84,7 +79,7 @@ def compress(img, source_size, destination_size, step):
             D = img[i*destination_size:(i+1)*destination_size,j*destination_size:(j+1)*destination_size]
             # Test all possible transformations and take the best one
             for k, l, direction, angle, S in transformed_blocks:
-                contrast, brightness = find_contrast_and_brightness2(D, S)
+                contrast, brightness = encontrar_brillo_contraste(D, S)
                 S = contrast*S + brightness
                 d = np.sum(np.square(D - S))
                 if d < min_d:
@@ -104,8 +99,8 @@ def decompress(transformations, source_size, destination_size, step, nb_iter=8):
             for j in range(len(transformations[i])):
                 # Apply transform
                 k, l, flip, angle, contrast, brightness = transformations[i][j]
-                S = reduce(iterations[-1][k*step:k*step+source_size,l*step:l*step+source_size], factor)
-                D = apply_transformation(S, flip, angle, contrast, brightness)
+                S = reducir(iterations[-1][k*step:k*step+source_size,l*step:l*step+source_size], factor)
+                D = aplicar_transformacion(S, flip, angle, contrast, brightness)
                 cur_img[i*destination_size:(i+1)*destination_size,j*destination_size:(j+1)*destination_size] = D
         iterations.append(cur_img)
         cur_img = np.zeros((height, width))
@@ -114,14 +109,14 @@ def decompress(transformations, source_size, destination_size, step, nb_iter=8):
 # Compression for color images
 
 def reduce_rgb(img, factor):
-    img_r, img_g, img_b = extract_rgb(img)
-    img_r = reduce(img_r, factor)
-    img_g = reduce(img_g, factor)
-    img_b = reduce(img_b, factor)
-    return assemble_rbg(img_r, img_g, img_b)
+    img_r, img_g, img_b = extraer_rgb(img)
+    img_r = reducir(img_r, factor)
+    img_g = reducir(img_g, factor)
+    img_b = reducir(img_b, factor)
+    return construir_rgb(img_r, img_g, img_b)
 
 def compress_rgb(img, source_size, destination_size, step):
-    img_r, img_g, img_b = extract_rgb(img)
+    img_r, img_g, img_b = extraer_rgb(img)
     return [compress(img_r, source_size, destination_size, step), \
         compress(img_g, source_size, destination_size, step), \
         compress(img_b, source_size, destination_size, step)]
@@ -130,7 +125,7 @@ def decompress_rgb(transformations, source_size, destination_size, step, nb_iter
     img_r = decompress(transformations[0], source_size, destination_size, step, nb_iter)[-1]
     img_g = decompress(transformations[1], source_size, destination_size, step, nb_iter)[-1]
     img_b = decompress(transformations[2], source_size, destination_size, step, nb_iter)[-1]
-    return assemble_rbg(img_r, img_g, img_b)
+    return construir_rgb(img_r, img_g, img_b)
 
 # Plot
 
@@ -163,8 +158,8 @@ candidates = [[direction, angle] for direction in directions for angle in angles
 
 def test_greyscale():
     img = mpimg.imread('monkey.gif')
-    img = get_greyscale_image(img)
-    img = reduce(img, 4)
+    img = escala_grises(img)
+    img = reducir(img, 4)
     plt.figure()
     plt.imshow(img, cmap='gray', interpolation='none')
     transformations = compress(img, 8, 4, 8)
